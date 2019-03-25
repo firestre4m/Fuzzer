d3b06aabec274eb11be36b4e118f40024bae6ced
quadrupleflame <harrisonhuang1025@gmail.com>
2019-03-25 17:28:43 -0400

    APP random & command line

--- APPFuzzer.py
+++ APPFuzzer.py
@@ -13,16 +13,31 @@ class APPFuzzer(TCPSession):
 		self.invalid = 0
 
 
-	def build_default(self):
-		for i in range(100):
-			# payload = "\x00"* random.randint(0,9) + "\x33"
-			payload = 'a'*random.randint(0,5) + 'b'
-			payload = bytes(payload, encoding = 'utf-8')
-			self.defaultset.append(payload)
+	def build_default(self, number, size):
+		
+		if isinstance(size, int):
+			for i in range(number):
+				payload = b''
+				for j in range(size):
+					payload += bytes([random.randint(0, 255)])
+				# payload = 'a'*size
+				# payload = bytes(payload, encoding = 'utf-8')
+				self.defaultset.append(payload)
+
+		elif isinstance(size, tuple):
+			for i in range(number):
+				payload = b''
+				s = random.randint(size[0],size[1])
+				for j in range(s):
+					payload += bytes([random.randint(0, 255)])
+				# payload = "\x00"* random.randint(0,9) + "\x33"
+				# payload = 'a'*random.randint(size[0],size[1])
+				# payload = bytes(payload, encoding = 'utf-8')
+				self.defaultset.append(payload)
 
 
-	def default_run(self):
-		self.build_default()
+	def default_run(self, number, size):
+		self.build_default(number, size)
 
 		self.connect()
 		for p in self.defaultset:
@@ -49,12 +64,12 @@ if __name__ == '__main__':
 	src_ip = "10.0.2.15"
 	dst_ip = "192.168.0.26"
 	# dst_ip = "129.236.238.135"
-	dport = 9998
+	dport = 9999
 	sport = 7890
 	print("[*]Begin APPFuzzer, presss CTRL-C to terminate")
 	try:
 		fuzzer = APPFuzzer(src_ip, dst_ip, sport, dport)
-		fuzzer.default_run()
+		fuzzer.default_run(10, 5)
 		print("[+]Finished, {valid} valid, {invalid} invalid".format(valid = fuzzer.valid, invalid = fuzzer.invalid))
 	except KeyboardInterrupt:
 		fuzzer.close()
--- Fuzzer.py
+++ Fuzzer.py
@@ -4,6 +4,7 @@ from IPFuzzer import IPFuzzer
 from TCPFuzzer import TCPFuzzer
 from APPFuzzer import APPFuzzer
 from time import sleep
+import re
 
 def check_args(args):
 	if args.layer in ['IP', 'TCP']:
@@ -28,10 +29,27 @@ def check_args(args):
 		if args.file and (args.number or args.size):
 			print("please choose to default run or run from file")
 			sys.exit(0)
+		# remember to check validation of size
+		if args.size:
+			pattern = re.compile('^\d+$|^\d+-\d+$')
+			if not re.match(pattern, args.size):
+				print("size argument is wrong")
+				sys.exit(0)
+
 		if not args.number:
 			args.number = 10
 		if not args.size:
-			args.size = 10
+			args.size = "10"
+
+def get_size(size_str):
+	size_input = size_str.split('-')
+	if len(size_input) == 1:
+		return int(size_input[0])
+	elif len(size_input) == 2:
+		return (int(size_input[0]), int(size_input[1]))
+	else:
+		print("wrong size input")
+		sys.exit(0)
 
 def run(args):
 	src_ip = "10.0.2.15"
@@ -69,6 +87,16 @@ def run(args):
 	else:
 		if not args.file:
 			print("APP default run")
+			size = get_size(args.size)
+			try:
+				fuzzer = APPFuzzer(src_ip, dst_ip, sport, dport)
+				fuzzer.default_run(args.number, size)
+				print("[+]Finished, {valid} valid, {invalid} invalid".format(valid = fuzzer.valid, invalid = fuzzer.invalid))
+			except KeyboardInterrupt:
+				fuzzer.close()
+				sleep(0.2)
+				print("[*]Terminated!")
+				sys.exit(0)
 		else:
 			print("APP run from file")
 
@@ -82,7 +110,7 @@ if __name__ == '__main__':
 	parser.add_argument('-l', '--layer', dest = 'layer', choices = {'IP', 'TCP', 'APP'}, required = True, help = 'layer to be fuzzed, can be IP/TCP/APP')
 	parser.add_argument('-t', '--target_field', dest = 'field', help = 'field to be fuzzed')
 	parser.add_argument('-n', '--number', dest = 'number', type = int, choices = range(0, 51), help = 'number of tests to run')
-	parser.add_argument('-s', '--size', dest = 'size', type = int, choices = range(0, 101), help = 'size of payload')
+	parser.add_argument('-s', '--size', dest = 'size', type = str, help = 'size of payload')
 	parser.add_argument('-f', '--file', dest = 'file', help = 'file containing tests')
 	args = parser.parse_args()
 
